b92f201fb51e0a827e6912426bc2b48e0f96249a
Aditya <102513627+Adityapng@users.noreply.github.com>
2024-07-08 10:46:45 +0530

    added vercel.json file with changes part 3

--- src/components/imageCarausal.jsx
+++ src/components/imageCarausal.jsx
@@ -92,28 +92,28 @@ const ImageCarausal = () => {
           <div className="w-full">
             <img
               className="w-full h-[600px] min-[481px]:max-[1180px]:h-[500px] min-[200px]:max-[480px]:h-[250px] object-cover rounded-3xl"
-              src="./public/assets/elements/ImageCarausal/1.jpg"
+              src="/public/assets/elements/ImageCarausal/1.jpg"
               alt="Caraousal"
             />
           </div>
           <div className="w-full">
             <img
               className="w-full h-[600px] min-[481px]:max-[1180px]:h-[500px] min-[200px]:max-[480px]:h-[250px] object-cover rounded-3xl"
-              src="./public/assets/elements/ImageCarausal/2.jpg"
+              src="/public/assets/elements/ImageCarausal/2.jpg"
               alt="Caraousal"
             />
           </div>
           <div className="w-full">
             <img
               className="w-full h-[600px] min-[481px]:max-[1180px]:h-[500px] min-[200px]:max-[480px]:h-[250px] object-cover rounded-3xl"
-              src="./public/assets/elements/ImageCarausal/3.jpg"
+              src="/public/assets/elements/ImageCarausal/3.jpg"
               alt="Caraousal"
             />
           </div>
           <div className="w-full">
             <img
               className="w-full h-[600px] min-[481px]:max-[1180px]:h-[500px] min-[200px]:max-[480px]:h-[250px] object-cover rounded-3xl"
-              src="./public/assets/elements/ImageCarausal/2.jpg"
+              src="/public/assets/elements/ImageCarausal/2.jpg"
               alt="Caraousal"
             />
           </div>
