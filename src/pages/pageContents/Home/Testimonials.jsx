9108d55a3e2b0a48eb018dcc1b342f366929a455
Aditya <102513627+Adityapng@users.noreply.github.com>
2024-08-19 11:17:32 +0530

    changed image in testimonial 2.1

--- src/pages/pageContents/Home/Testimonials.jsx
+++ src/pages/pageContents/Home/Testimonials.jsx
@@ -10,7 +10,7 @@ const itokaStudent = "/assets/testimonials/students/itoka.JPG";
 const loangamStudent = "/assets/testimonials/students/loangam.JPG";
 const ngunnonStudent = "/assets/testimonials/students/ngunnon.JPG";
 const somStudent = "/assets/testimonials/students/som.JPG";
-const kedarai = "/assets/testimonials/students/kedaraistudent.jpeg";
+const kedarai = "/assets/testimonials/students/kedarai.jpeg";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
